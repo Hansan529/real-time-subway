770e259ea4bd8ae15cc5ffc82be44127c5c22ec2
hansan529 <hansan0529@gmail.com>
2023-08-30 00:56:21 +0900

    feat: 지하철 역 정보 STATION_ORDER 적용
    기존 2호선의 경우 인천2호선과 같이 결과가 반환되어 총 역 개수가 맞지 않아
    02호선으로 교체하였음, 경의중앙선의 경우 역 순서가 일치하지 않아 임의대로 수정하기 위해
    sTATION_ORDER를 차용함

--- src/app/api/line-info/route.ts
+++ src/app/api/line-info/route.ts
@@ -22,12 +22,15 @@ export interface Station {
 
 export async function GET(req: NextRequest) {
   let line: string = req.nextUrl.searchParams.get('line')?.trim() as string;
+  const originalLineName = line.slice();
   switch (line) {
     case '우이신설선':
       line = '우이신설경전철';
       break;
     case '경의중앙선':
       line = '경의선';
+    case '2호선':
+      line = '02호선';
       break;
     default:
       break;
@@ -44,17 +47,18 @@ export async function GET(req: NextRequest) {
 
     /** 데이터베이스 접속 */
     const dbconnection: any = await mysql.createConnection(mysqlConnect);
+    /** 02호선 -> 2호선 변경 작업 */
+    if (line.charAt(0) === '0') line = line.slice(1);
     /** 지하철 노선에 맞게 역 개수 확인 */
     const [queryResults] = await dbconnection.execute(
-      `SELECT LIST_TOTAL_COUNT FROM line_info WHERE LINE_NUM = "${line}" LIMIT 1`,
+      `SELECT TOTAL_COUNT FROM line_${line} LIMIT 1`,
       []
     );
-
     /** 역 정보가 일치하지 않을 경우 추가 */
     // if (Object.values(queryResults[0])[0] !== res.list_total_count) {
     /** 이미 있는 역들은 total_count가 업데이트되고, 없는 역은 추가한다 */
     res.row.map(async (data: Station) => {
-      /** 첫번째 문자열을 숫자로 변경이 가능한지, 가능하면 TRUE, 불가능하면 FALSE */
+      /** res에서 받은 01, 02호선 첫번째 문자열을 숫자로 변경이 가능한지, 가능하면 TRUE, 불가능하면 FALSE */
       const numberChk = !isNaN(Number(data.LINE_NUM.charAt(0)));
 
       let lineNum = data.LINE_NUM;
@@ -62,12 +66,12 @@ export async function GET(req: NextRequest) {
       /** 만약 숫자일 경우, 맨 앞 숫자 제거 (0 제거하기 위한 수식) */
       if (numberChk) lineNum = data.LINE_NUM.slice(1);
 
-      await dbconnection.execute(`INSERT INTO line_info(LIST_TOTAL_COUNT, STATION_CD, STATION_NM, STATION_NM_ENG, LINE_NUM, FR_CODE, STATION_NM_CHN, STATION_NM_JPN)
-      VALUES(${res.list_total_count}, "${data.STATION_CD}", "${data.STATION_NM}", "${data.STATION_NM_ENG}", "${lineNum}", "${data.FR_CODE}", "${data.STATION_NM_CHN}", "${data.STATION_NM_JPN}") ON DUPLICATE KEY UPDATE LIST_TOTAL_COUNT = ${res.list_total_count}`);
+      await dbconnection.execute(`INSERT INTO line_${lineNum}(TOTAL_COUNT, STATION_CD, STATION_NM, STATION_NM_ENG, LINE_NUM, FR_CODE, STATION_NM_CHN, STATION_NM_JPN, STATION_ORDER)
+      SELECT ${res.list_total_count}, "${data.STATION_CD}", "${data.STATION_NM}", "${data.STATION_NM_ENG}", "${lineNum}", "${data.FR_CODE}", "${data.STATION_NM_CHN}", "${data.STATION_NM_JPN}", IFNULL(MAX(STATION_ORDER), 0) + 1 FROM line_${lineNum} ON DUPLICATE KEY UPDATE TOTAL_COUNT = ${res.list_total_count}`);
     });
     // }
     /** 업데이트한 목록을 검색 */
-    const query = `SELECT * FROM line_info WHERE line_num = "${line}"`;
+    const query = `SELECT * FROM line_${line} WHERE line_num = "${line}"`;
     const [results] = await dbconnection.execute(query, values);
     dbconnection.end();
     return NextResponse.json(results);
